787988c01eab7500813ecebee797910bc3e9319e
Chris Stoll <stoll101@gmail.com>
2021-11-24 23:49:05 -0500

    Made changes to ArchivePermission class

--- Square9APIHelperLibrary/DataTypes/ArchivePermission.cs
+++ Square9APIHelperLibrary/DataTypes/ArchivePermission.cs
@@ -10,31 +10,7 @@ namespace Square9APIHelperLibrary.DataTypes
     {
         public ArchivePermission(int level = 0) 
         {
-            string permissionLevel = Convert.ToString(level, 2);
-            while (permissionLevel.Length < 20)
-            {
-                permissionLevel = $"0{permissionLevel}";
-            }
-            FullAPIAccess = (permissionLevel[0] == '1') ? true: false;
-            DeleteBatches = (permissionLevel[1] == '1') ? true : false;
-            MoveDoc = (permissionLevel[2] == '1') ? true: false;
-            ModifyPages = (permissionLevel[3] == '1') ? true: false;
-            PublishRevisions = (permissionLevel[4] == '1') ? true: false;
-            ViewDocRevision = (permissionLevel[5] == '1') ? true: false;
-            LaunchDocCopy = (permissionLevel[6] == '1') ? true: false;
-            LaunchDoc = (permissionLevel[7] == '1') ? true: false;
-            ModifyAnnotations = (permissionLevel[8] == '1') ? true: false;
-            ModifyData = (permissionLevel[9] == '1') ? true: false;
-            ViewDocHistory = (permissionLevel[10] == '1') ? true: false;
-            ViewInAcrobat = (permissionLevel[11] == '1') ? true: false;
-            ExportDocs = (permissionLevel[12] == '1') ? true: false;
-            ExportData = (permissionLevel[13] == '1') ? true: false;
-            Email = (permissionLevel[14] == '1') ? true: false;
-            Print = (permissionLevel[15] == '1') ? true: false;
-            Delete = (permissionLevel[16] == '1') ? true: false;
-            ModifyDocument = (permissionLevel[17] == '1') ? true: false;
-            Add = (permissionLevel[18] == '1') ? true: false;
-            View = (permissionLevel[19] == '1') ? true: false;
+            UpdateBools(level);
             CalculatePermissionLevel();
         }
         public int Level { get; set; }
@@ -59,7 +35,7 @@ namespace Square9APIHelperLibrary.DataTypes
         public bool DeleteBatches { get; set; }
         public bool FullAPIAccess { get; set; }
 
-        public void CalculatePermissionLevel()
+        public void CalculatePermissionLevel() //Calculates Level based on Bools
         {
             string permissionLevel = "";
             bool[] permissions = { FullAPIAccess, DeleteBatches, MoveDoc, ModifyPages, PublishRevisions, ViewDocRevision, LaunchDocCopy, LaunchDoc, ModifyAnnotations, ModifyData, ViewDocHistory, ViewInAcrobat, ExportDocs, ExportData, Email, Print, Delete, ModifyDocument, Add, View};
@@ -69,52 +45,42 @@ namespace Square9APIHelperLibrary.DataTypes
             }
             Level = Convert.ToInt32(permissionLevel, 2);
         }
+        private void UpdateBools(int level) //Sets bools bases on passed level
+        {
+            string permissionLevel = Convert.ToString(level, 2);
+            while (permissionLevel.Length < 20)
+            {
+                permissionLevel = $"0{permissionLevel}";
+            }
+            FullAPIAccess = (permissionLevel[0] == '1') ? true : false;
+            DeleteBatches = (permissionLevel[1] == '1') ? true : false;
+            MoveDoc = (permissionLevel[2] == '1') ? true : false;
+            ModifyPages = (permissionLevel[3] == '1') ? true : false;
+            PublishRevisions = (permissionLevel[4] == '1') ? true : false;
+            ViewDocRevision = (permissionLevel[5] == '1') ? true : false;
+            LaunchDocCopy = (permissionLevel[6] == '1') ? true : false;
+            LaunchDoc = (permissionLevel[7] == '1') ? true : false;
+            ModifyAnnotations = (permissionLevel[8] == '1') ? true : false;
+            ModifyData = (permissionLevel[9] == '1') ? true : false;
+            ViewDocHistory = (permissionLevel[10] == '1') ? true : false;
+            ViewInAcrobat = (permissionLevel[11] == '1') ? true : false;
+            ExportDocs = (permissionLevel[12] == '1') ? true : false;
+            ExportData = (permissionLevel[13] == '1') ? true : false;
+            Email = (permissionLevel[14] == '1') ? true : false;
+            Print = (permissionLevel[15] == '1') ? true : false;
+            Delete = (permissionLevel[16] == '1') ? true : false;
+            ModifyDocument = (permissionLevel[17] == '1') ? true : false;
+            Add = (permissionLevel[18] == '1') ? true : false;
+            View = (permissionLevel[19] == '1') ? true : false;
+        }
         public void SelectAll()
         {
-            FullAPIAccess = true;
-            DeleteBatches = true;
-            MoveDoc = true;
-            ModifyPages = true;
-            PublishRevisions = true;
-            ViewDocRevision = true;
-            LaunchDocCopy = true;
-            LaunchDoc = true;
-            ModifyAnnotations = true;
-            ModifyData = true;
-            ViewDocHistory = true;
-            ViewInAcrobat = true;
-            ExportDocs = true;
-            ExportData = true;
-            Email = true;
-            Print = true;
-            Delete = true;
-            ModifyDocument = true;
-            Add = true;
-            View = true;
+            UpdateBools(Convert.ToInt32("11111111111111111111", 2));
             CalculatePermissionLevel();
         }
         public void ClearAll()
         {
-            FullAPIAccess = false;
-            DeleteBatches = false;
-            MoveDoc = false;
-            ModifyPages = false;
-            PublishRevisions = false;
-            ViewDocRevision = false;
-            LaunchDocCopy = false;
-            LaunchDoc = false;
-            ModifyAnnotations = false;
-            ModifyData = false;
-            ViewDocHistory = false;
-            ViewInAcrobat = false;
-            ExportDocs = false;
-            ExportData = false;
-            Email = false;
-            Print = false;
-            Delete = false;
-            ModifyDocument = false;
-            Add = false;
-            View = false;
+            UpdateBools(0);
             CalculatePermissionLevel();
         }
     }
